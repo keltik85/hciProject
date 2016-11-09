b05587b7b90b0b0c045f467569a843530b3f66ab
altug <altug@altug.al>
2016-11-09 08:10:59 +0100

    insert statements from phones.json file for mongodb

--- src/main/java/my/app/main/Application.java
+++ src/main/java/my/app/main/Application.java
@@ -3,7 +3,6 @@ package my.app.main;
 
 import com.mongodb.Mongo;
 import my.app.model.angular.Phone;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.core.MongoClientFactoryBean;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.io.File;
-import java.io.InputStream;
-import java.nio.charset.Charset;
+import java.io.IOException;
 
 @SpringBootApplication
 @ComponentScan({"my.app.*"})
@@ -51,36 +46,30 @@ public class Application {
         return mongoOps;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        prepareMongoDBData();
+        SpringApplication.run(Application.class, args);
+    }
 
-        try {
-            /*
-            mongoOps.insert(new Phone("1", 123, "asd", "dfg", "cvbx"));
-            mongoOps.insert(new Phone("2", 456, "qweqwe", "23423", "khjk"));
-            mongoOps.insert(new Phone("3", 890890, "ghjfgj", "jfghjfg", "453275"));
-            */
-
-            JSONArray phones = new JSONArray(
-                    IOUtils.toString(
-                            Application.class.getResourceAsStream("../../../static/misc/phones.json"), "utf-8"));
-
-
-            System.out.println("phones.length() ---> " + phones.length());
-            System.out.println("phones ---> " + phones);
-
-            for (int i = 0; i < phones.length(); i++) {
-                String currId = phones.getJSONObject(i).getString("id");
-                Integer currAge = Integer.valueOf(phones.getJSONObject(i).getString("age"));
-                String currSnippet = phones.getJSONObject(i).getString("snippet");
-                String currName = phones.getJSONObject(i).getString("name");
-                String currImageUrl = phones.getJSONObject(i).getString("imageUrl");
-                //mongoOps.insert(new Phone(currId, currAge, currSnippet, currName, currImageUrl));
-            }
-
-            //SpringApplication.run(Application.class, args);
-        } catch (Exception ex) {
-            System.out.println("exception when inserting the stuff:\n\n" + ex.toString());
+    private static void prepareMongoDBData() throws IOException {
+        JSONArray phones = new JSONArray(
+                IOUtils.toString(
+                        Application.class.getResourceAsStream("../../../static/misc/phones.json"), "utf-8"));
+        for (int i = 0; i < phones.length(); i++) {
+            String currId = phones.getJSONObject(i).getString("id");
+            Integer currAge = Integer.valueOf(phones.getJSONObject(i).getString("age"));
+            String currSnippet = phones.getJSONObject(i).getString("snippet");
+            String currName = phones.getJSONObject(i).getString("name");
+            String currImageUrl = phones.getJSONObject(i).getString("imageUrl");
+            insertIntoMongo(currId, currAge, currSnippet, currName, currImageUrl);
         }
+    }
 
+    private static void insertIntoMongo(String currId, Integer currAge, String currSnippet, String currName, String currImageUrl) {
+        try {
+            mongoOps.insert(new Phone(currId, currAge, currSnippet, currName, currImageUrl));
+        } catch (Exception ex) {
+            System.out.println("mongoOps.insert failed:\n" + ex.toString());
+        }
     }
 }
