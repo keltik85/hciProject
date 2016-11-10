af6834d5397c1a17fd5d43d4f1c6430485dfe399
altug <altug@altug.al>
2016-11-10 22:24:11 +0100

    detail view front end

--- src/main/resources/static/js/my_components/phone-detail/phone-detail.component.js
+++ src/main/resources/static/js/my_components/phone-detail/phone-detail.component.js
@@ -4,10 +4,13 @@
 angular.
   module('phoneDetail').
   component('phoneDetail', {
-    template: 'TBD: Detail view for <span>{{$ctrl.phoneId}}</span>',
-    controller: ['$routeParams',
+    template: 'js/my_components/phone-detail/phone-detail.template.html',
+    controller: ['$http', '$routeParams',
       function PhoneDetailController($routeParams) {
-        this.phoneId = $routeParams.phoneId;
+        var self = this;
+        $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
+         self.phone = response.data;
+        });
       }
     ]
   });
